34635bf5c9e3581d83852e7a1c576acb42e37d33
HristoHristov <hristo_d._hristov@mail.bg>
2014-10-28 00:23:41 +0200

    Finished Header

--- header.php
+++ header.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php $title; echo $title;?></title>       
-     <?php
-        $variable=$css;
-        $js_link;
-        $array=explode(',', $variable);
-        foreach ($array as  $value) {
-            echo $value;
-        }       
-        if(!empty($js_link)) {
-            echo $js_link;
-        }
-    ?>
+    <title><?php $title; echo $title;?></title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link rel="stylesheet" href="../css/styles.css"/>
 </head>
 <body>
