2f346a94df67296cf37d32f558c365facd66b827
HristoHristov <hristo_d._hristov@mail.bg>
2014-10-27 14:56:04 +0200

    website footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+$title='Footer';
+$css='<link href="css/bootstrap.min.css" rel="stylesheet">, <link rel="stylesheet" href="css/styles.css"/>, <link rel="stylesheet" href="css/footer-styles.css"/>';
+include 'header.php';
+?>
+<footer>
+    <img src="img/BB-logo3.png" class="img-responsive" alt=""/>
+    <ul>
+        <li><a href="About_us.html" target="_blank">About Us</a></li>
+        <li>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+                <img class="validator" src="http://www.w3c.it/events/2013/css3html5/img/html5-logo.png" alt="Валиден HTML5!" />
+            </a>
+            </li>
+        <li><p>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+                <img class="validator" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Валиден CSS!" />
+            </a>
+        </p></li>
+        <li><a href="https://softuni.bg/" target="_blank">Friends</a></li>
+    </ul>
+</footer>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
