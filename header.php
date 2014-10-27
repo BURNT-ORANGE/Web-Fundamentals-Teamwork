efd4aa02e455a2bc7f183a6d0cfa13d5e72c8efb
HristoHristov <hristo_d._hristov@mail.bg>
2014-10-28 01:37:14 +0200

    final header

--- header.php
+++ header.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php $title; echo $title;?></title>
+    <title><?php $title; echo $title;?></title>   
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <link rel="stylesheet" href="../css/styles.css"/>
+    <link rel="stylesheet" href="../css/styles.css"/>      
+     <?php        
+        $footer;           
+        if(!empty($footer)) {
+            echo $footer;
+        }
+    ?>
 </head>
 <body>
